cd4ad8689358fa8835c8205b7893a5d0f5a5b119
Alberto Rodriguez <juniorrodriguez@Albertos-iMac.local>
2019-04-30 21:32:00 -0500

    added event listeners

--- js/index.js
+++ js/index.js
@@ -5,4 +5,36 @@ btn.forEach(function(e){
     })
 });
 
+//Used click to change the background on the buttons to red and then dblclick to change back to default color
+const button = document.querySelectorAll('.btn');
+button.forEach(function(element){
+    element.addEventListener('click', function(e){
+        // console.log('click works!')
+        e.target.style.background = 'black';
+    })
+   button.forEach(function(element){
+       element.addEventListener('dblclick', function(e){
+        //    console.log('dblclick works!')
+           e.target.style.background = '';
+       })
+   }) 
 
+   button.forEach(function(element){
+
+   })
+});
+
+// Used Mouseover and Mouseleave (changes color of p tags to blue when mouse hovers over and back to black when mouse isnt hovering over.)
+const input = document.querySelectorAll('p');
+input.forEach(function(element){
+    element.addEventListener('mouseover', function(e){
+        // console.log('mouseover works!')
+        e.target.style.color = 'blue';
+    })
+    input.forEach(function(element){
+        element.addEventListener('mouseleave', function(e){
+            // console.log('mouseleave works!')
+            e.target.style.color = '';
+        })
+    })
+ });
